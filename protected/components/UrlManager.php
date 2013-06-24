3e3fc8e825ac8dec25c12d3eb4be5a037ff1ecc2
Fei Wang <wangfei001@hotmail.com>
2013-06-24 22:53:00 +1000

    multiple languages

---
+++ protected/components/UrlManager.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: wangfei0001
+ * Date: 13-6-24
+ * Time: PM10:07
+ * To change this template use File | Settings | File Templates.
+ */
+class UrlManager
+{
+
+}
